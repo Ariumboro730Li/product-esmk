2407db036f10bde47cea67e0790fa8279c0f8ab2
tigin <mtiginsawala23@gmail.com>
2025-02-10 10:27:31 +0700

    add role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Auth\Role;
+use Carbon\Carbon;
 use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,69 +15,27 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $array =
-        [
-            [
-                "id" => 1,
-                "guard_name" => "employee",
-                "name" => "Super Admin",
-                "is_active" => 1,
-                "created_at" => "2023-11-30 02:40:38",
-                "updated_at" => "2023-11-30 02:40:38",
-                "deleted_at" => null
-            ],
-            [
-                "id" => 2,
-                "guard_name" => "employee",
-                "name" => "Admin",
-                "is_active" => 1,
-                "created_at" => "2023-11-30 02:40:38",
-                "updated_at" => "2023-11-30 02:40:38",
-                "deleted_at" => null
-            ],
-            [
-                "id" => 3,
-                "guard_name" => "employee",
-                "name" => "Assessor",
-                "is_active" => 1,
-                "created_at" => "2023-11-30 02:40:38",
-                "updated_at" => "2023-11-30 02:40:38",
-                "deleted_at" => null
-            ],
-            [
-                "id" => 4,
-                "guard_name" => "employee",
-                "name" => "Ketua Tim",
-                "is_active" => 1,
-                "created_at" => "2023-11-30 02:40:38",
-                "updated_at" => "2023-11-30 02:40:38",
-                "deleted_at" => null
-            ],
-            [
-                "id" => 5,
-                "guard_name" => "employee",
-                "name" => "Kasubdit",
-                "is_active" => 1,
-                "created_at" => "2023-11-30 02:40:38",
-                "updated_at" => "2023-11-30 02:40:38",
-                "deleted_at" => null
-            ],
-            [
-                "id" => 6,
-                "guard_name" => "employee",
-                "name" => "Direktur",
-                "is_active" => 1,
-                "created_at" => "2023-11-30 02:40:38",
-                "updated_at" => "2023-11-30 02:40:38",
-                "deleted_at" => null
-            ]
+        $roles = [
+            ["id" => 1, "guard_name" => "employee", "name" => "Super Admin"],
+            ["id" => 2, "guard_name" => "employee", "name" => "Admin"],
+            ["id" => 3, "guard_name" => "employee", "name" => "Assessor"],
+            ["id" => 4, "guard_name" => "employee", "name" => "Ketua Tim"],
+            ["id" => 5, "guard_name" => "employee", "name" => "Kasubdit"],
+            ["id" => 6, "guard_name" => "employee", "name" => "Direktur"],
+            ["id" => 7, "guard_name" => "employee", "name" => "Ketua tim kota"],
+            ["id" => 8, "guard_name" => "employee", "name" => "Ketua tim provinsi"],
+            ["id" => 9, "guard_name" => "employee", "name" => "Assessor kota"]
         ];
 
-        if (DB::table('roles')->count() > 0) {
-            return;
+        foreach ($roles as $role) {
+            Role::updateOrCreate(
+                ['name' => $role['name'], 'guard_name' => $role['guard_name']], // Cek berdasarkan name dan guard_name
+                [
+                    'is_active' => 1,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ]
+            );
         }
-
-        DB::table('roles')->insert($array);
-
     }
 }
