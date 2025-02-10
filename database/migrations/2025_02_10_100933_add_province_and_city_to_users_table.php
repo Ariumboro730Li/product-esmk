c2598b8f59392dd0b9257f9f717afce6f1220aa8
tigin <mtiginsawala23@gmail.com>
2025-02-10 10:45:32 +0700

    add migration city and province on user

---
+++ database/migrations/2025_02_10_100933_add_province_and_city_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('province_id')->after('is_active')->nullable();
+            $table->bigInteger('city_id')->after('is_active')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('province_id');
+            $table->dropColumn('city_id');
+        });
+    }
+};
